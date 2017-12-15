7404db5d39d401f737000925068ee9b394ee469b
qwertyuiop6 <super771783057@foxmail.com>
2017-12-15 16:16:26 +0800

    :zap:

--- public/js/loadCss.js
+++
@@ -1,1 +0,0 @@
-define([],function(){function loadCss(url,callback){var link=document.createElement("link");link.type="text/css",link.rel="stylesheet",link.id="loadCss-"+Date.now(),link.href=url,link.onload=callback,head.appendChild(link)}var head=document.getElementsByTagName("head")[0];return loadCss});
